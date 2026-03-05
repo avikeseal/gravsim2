d5563bf9e9aa546090d13a96401739bc2f31d6cd
avikeseal <avikeseal@gmail.com>
2026-03-05 14:42:59 -0500

    added a selection scrreen function to help users choose between available options

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ FPS = 60
 
 #initializing pygame:
 pygame.init()
+screen = pygame.display.set_mode()
 pygame.font.init()
 
 #colors:
@@ -84,6 +85,42 @@ class Body:
         pygame.draw.circle(screen, self.color, (int(self.pos[0]), int(self.pos[1])), self.radius)
         if len(self.path) > 1:
             pygame.draw.lines(screen, YELLOW, False, self.path, 2)
+
+#selection screen:
+
+#takes the pygame screen to draw on and prompt string so you can reuse the same function twice
+def selection_screen(screen, prompt):
+    screen.fill((0, 0, 0))  
+    font = pygame.font.SysFont(None, 48)
+    title = font.render(prompt, True, (255, 255, 255))
+    screen.blit(title, (100, 50))
+
+    small_font = pygame.font.SysFont(None, 36)
+    #drawing options
+    #pulls the name out of the dict and loops thru them to render each one 
+    #on screen with a number next to it.
+    #enumerate gives us both value and index at the same time
+    bodies_list = list(BODIES.keys())
+    for i, name in enumerate(bodies_list):
+        text = small_font.render(f"{i+1}. {name}", True, (200, 200, 200))
+        screen.blit(text, (100, 150 + i * 50))
+
+    pygame.display.flip()
+
+
+    #listen for keypress:
+    #waiting loop - listens for a user key press
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                #keypress detection:
+                #subtraction converts the keycode into an actual index:
+                if pygame.K_1 <= event.key <= pygame.K_9:
+                    index = event.key - pygame.K_1
+                    if index < len(bodies_list):
+                        #once a valid key key is pressed the function 
+                        #returns the names of the two chosen bodies
+                        return bodies_list[index]
 
 
 #main game loop variable:
